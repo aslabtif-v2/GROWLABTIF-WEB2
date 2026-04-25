91a0d072c8622c55b733535bb30e732f992ef895
Naurah <nrph2005@gmail.com>
2026-04-25 18:11:50 +0700

    sudah melengkapi models category item trasaction_detail

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'stok',
+        'price',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+    public function transaction_detail()
+    {
+        return $this->hasMany(transaction_detail::class, 'item_id', 'id');
+    }
 }
--- app/Models/Transaction_detail.php
+++ app/Models/Transaction_detail.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction_detail extends Model
 {
-    //
+    protected $fillable = ['transaction_id', 'item_id', 'quantity', 'total_price'];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id', 'id');
+    }
+    public function item()
+    {
+        return $this->belongsTo(Item::class, 'item_id', 'id');
+    }
 }
